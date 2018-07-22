fbe57542ac1844817bc514f45ceed4d0a89a8b66
Gu <as20757738@gmail.com>
2018-07-22 18:21:51 +0800

    tidy code

--- hook_function.cpp
+++ hook_function.cpp
@@ -5,7 +5,7 @@
 
 //68 ªª®v head
 //68 122
-#define chahead ((NumVertices == 68 && PrimitiveCount!=80) || (NumVertices == 122 && PrimitiveCount!=140) || NumVertices  == 114|| NumVertices == 282 || NumVertices == 74 || NumVertices == 194 || NumVertices == 34 || NumVertices == 26 || NumVertices == 158 ||NumVertices == 130 ||NumVertices == 254 ||NumVertices == 66 || NumVertices == 82 ||NumVertices == 50)
+#define characterHEAD ((NumVertices == 68 && PrimitiveCount!=80) || (NumVertices == 122 && PrimitiveCount!=140) || NumVertices  == 114|| NumVertices == 282 || NumVertices == 74 || NumVertices == 194 || NumVertices == 34 || NumVertices == 26 || NumVertices == 158 ||NumVertices == 130 ||NumVertices == 254 ||NumVertices == 66 || NumVertices == 82 ||NumVertices == 50)
 PDWORD Direct3D_VMTable = NULL;
 bool fCall = true;
 
@@ -27,7 +27,10 @@ IDirect3DPixelShader9 *Front, *Back;
 
 
 int aimheight = 0;
+bool isInitalized = false;
 D3DVIEWPORT9 g_ViewPort;
+
+class CDraw CDraw;
 
 
 struct ModelInfo_t
@@ -35,6 +38,12 @@ struct ModelInfo_t
 	D3DXVECTOR3 Position2D;
 	D3DXVECTOR3 Position3D;
 	float Distance;
+	D3DPRIMITIVETYPE Type;
+	INT BaseIndex; 
+	UINT MinIndex; 
+	UINT NumVertices; 
+	UINT StartIndex; 
+	UINT PrimitiveCount;
 };
 
 vector<ModelInfo_t*>ModelInfo;
@@ -68,38 +77,6 @@ void DrawPoint(LPDIRECT3DDEVICE9 pDevice, int baseX, int baseY, int baseW, int b
 	D3DRECT BarRect = { baseX, baseY, baseX + baseW, baseY + baseH };
 	pDevice->Clear(1, &BarRect, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, Cor, 0, 0);
 }
-
-void AddModel(LPDIRECT3DDEVICE9 pDevice)
-{
-	ModelInfo_t* pModel = new ModelInfo_t;
-	
-	pDevice->GetViewport(&g_ViewPort);
-	D3DXMATRIX pProjection, pView, pWorld;
-	D3DXVECTOR3 vOut(0, 0, 0), vIn(0, (float)aimheight, 1);
-
-	pDevice->GetVertexShaderConstantF(0, pProjection, 4);
-	pDevice->GetVertexShaderConstantF(230, pView, 4);
-
-	D3DXMatrixIdentity(&pWorld);
-
-	D3DXVECTOR3 VectorMiddle(0, 0, 0), ScreenMiddlee(0, 0, 0);
-	D3DXVec3Unproject(&VectorMiddle, &ScreenMiddlee, &g_ViewPort, &pProjection, &pView, &pWorld);
-
-	D3DXVec3Project(&vOut, &vIn, &g_ViewPort, &pProjection, &pView, &pWorld);
-	
-	float RealDistance = GetDistance(VectorMiddle.x, VectorMiddle.y, vIn.x, vIn.y) / 100 ;
-	
-	if (vOut.z < 1.0f)
-	{
-		pModel->Position2D.y = vOut.y;
-		pModel->Position2D.x = vOut.x;
-		pModel->Position2D.z = vOut.z;
-		pModel->Distance = RealDistance;
-	}
-
-	ModelInfo.push_back(pModel);
-}
-
 
 HRESULT WINAPI Direct3DCreate9_VMTable(VOID)
 {
@@ -168,6 +145,7 @@ HRESULT WINAPI CreateDevice_Detour(LPDIRECT3D9 Direct3D_Object, UINT Adapter, D3
 
 HRESULT WINAPI Reset_Detour(LPDIRECT3DDEVICE9 pDevice, D3DPRESENT_PARAMETERS* pPresentationParameters)
 {
+	CDraw.GetDevice(pDevice);
 	g_font_default->OnLostDevice();
 	g_font_default->OnResetDevice();
 
@@ -230,19 +208,63 @@ void wallhack_ghostChams(LPDIRECT3DDEVICE9 pDevice, D3DPRIMITIVETYPE Type, INT B
 	//Device_Interface->SetPixelShader(Back);
 }
 
-int minX, minY, minZ, minCrosshairDistance, foundnum=0;
-int mouseOffset_X, mouseOffset_Y;
-int ScreenCenterX = NULL, ScreenCenterY = NULL;
-float mouseSmooth=2.5;
-bool my_tool_active;
+void AddModel(LPDIRECT3DDEVICE9 pDevice, D3DPRIMITIVETYPE Type, INT BaseIndex, UINT MinIndex, UINT NumVertices, UINT StartIndex, UINT PrimitiveCount)
+{
+	ModelInfo_t* pModel = new ModelInfo_t;
+
+	pDevice->GetViewport(&g_ViewPort);
+
+	D3DXMATRIX pProjection, pView, pWorld;
+	D3DXVECTOR3 vOut(0, 0, 0), vIn(0, (float)aimheight, 1);
+
+	pDevice->GetVertexShaderConstantF(0, pProjection, 4);
+	pDevice->GetVertexShaderConstantF(230, pView, 4);
+
+	D3DXMatrixIdentity(&pWorld);
+
+	D3DXVECTOR3 VectorMiddle(0, 0, 0), ScreenMiddlee(0, 0, 0);
+	D3DXVec3Unproject(&VectorMiddle, &ScreenMiddlee, &g_ViewPort, &pProjection, &pView, &pWorld);
+
+	D3DXVec3Project(&vOut, &vIn, &g_ViewPort, &pProjection, &pView, &pWorld);
+
+	float RealDistance = GetDistance(VectorMiddle.x, VectorMiddle.y, vIn.x, vIn.y) / 100;
+
+	if (vOut.z < 1.0f)
+	{
+		pModel->Position2D.y = vOut.y;
+		pModel->Position2D.x = vOut.x;
+		pModel->Position2D.z = vOut.z;
+		pModel->Distance = RealDistance;
+		pModel->Type = Type;
+		pModel->BaseIndex = BaseIndex;
+		pModel->MinIndex = MinIndex;
+		pModel->NumVertices = NumVertices;
+		pModel->StartIndex = StartIndex;
+		pModel->PrimitiveCount = PrimitiveCount;
+	}
+
+	ModelInfo.push_back(pModel);
+}
+
+int foundnum = 0;
+float mouseOffset_X = 0, mouseOffset_Y = 0;
+float ScreenCenterX = NULL, ScreenCenterY = NULL;
+float mouseSmooth=2.5, minCrosshairDistance;
+
 
 HRESULT WINAPI EndScene_Detour(LPDIRECT3DDEVICE9 pDevice)
 {
 	if (texture_Red == NULL) GenerateTexture(pDevice, &texture_Red, D3DCOLOR_ARGB(255, 255, 0, 0));
 	if (texture_Black == NULL) GenerateTexture(pDevice, &texture_Black, D3DCOLOR_ARGB(255, 0, 0, 0));
-	if (g_font_default == NULL) D3DXCreateFont(pDevice, 15, 0, FW_NORMAL, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH | FF_DONTCARE, (LPCWSTR)"Arial", &g_font_default); //Create fonts
+	if (g_font_default == NULL)	D3DXCreateFont(pDevice, 15, 0, FW_NORMAL, 1, 0, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, ANTIALIASED_QUALITY, DEFAULT_PITCH | FF_DONTCARE, (LPCWSTR)"Arial", &g_font_default); //Create fonts
 
-	
+	if (fCall)
+	{
+		CreateMyShader(&Front, pDevice, 255, 0, 0, 255);
+		CreateMyShader(&Back, pDevice, 0, 255, 0, 255);
+		fCall = false;
+	}
+
 	if (d3dmenu.g_font == NULL) {
 		d3dmenu.g_font = g_font_default;
 		d3dmenu.additem("AVA Hack @2018", D3DCOLOR_ARGB(255, 255, 000, 000));
@@ -251,7 +273,6 @@ HRESULT WINAPI EndScene_Detour(LPDIRECT3DDEVICE9 pDevice)
 		d3dmenu.additem("[F5] ChangeName", D3DCOLOR_ARGB(255, 255, 000, 000));
 		d3dmenu.additem("[F6] QQMacro", D3DCOLOR_ARGB(255, 255, 000, 000));
 	}
-
 
 	if (menu)
 	{
@@ -269,6 +290,8 @@ HRESULT WINAPI EndScene_Detour(LPDIRECT3DDEVICE9 pDevice)
 
 	foundnum = 0;
 	minCrosshairDistance = 500;
+
+	ModelInfo_t* TargetModel = new ModelInfo_t;
 	if (ModelInfo.size() != NULL)
 	{
 		D3DDEVICE_CREATION_PARAMETERS cparams;
@@ -280,17 +303,23 @@ HRESULT WINAPI EndScene_Detour(LPDIRECT3DDEVICE9 pDevice)
 
 		for (size_t i = 0; i < ModelInfo.size(); i++)
 		{
-
+			
 			DrawPoint(pDevice, (int)ModelInfo[i]->Position2D.x, (int)ModelInfo[i]->Position2D.y, 4, 4, D3DCOLOR_XRGB(255, 0, 0));
-			PrintText(g_font_default, (int)ModelInfo[i]->Position2D.x, (int)ModelInfo[i]->Position2D.y - 25, D3DCOLOR_XRGB(255, 0, 0),
-				"%.1f",
+			PrintText(g_font_default, (int)ModelInfo[i]->Position2D.x, (int)ModelInfo[i]->Position2D.y - 35, D3DCOLOR_XRGB(255, 0, 0),
+				"%.1f m",
 				ModelInfo[i]->Distance);
-
+			
 			if (minCrosshairDistance > GetDistance(ModelInfo[i]->Position2D.x, ModelInfo[i]->Position2D.y, ScreenCenterX, ScreenCenterY)
 				&& GetDistance(ModelInfo[i]->Position2D.x, ModelInfo[i]->Position2D.y, ScreenCenterX, ScreenCenterY) < 300) {
-				minX = ModelInfo[i]->Position2D.x;
-				minY = ModelInfo[i]->Position2D.y;
-				minZ = ModelInfo[i]->Position2D.z;
+				TargetModel->Position2D.x = ModelInfo[i]->Position2D.x;
+				TargetModel->Position2D.y = ModelInfo[i]->Position2D.y;
+				TargetModel->Position2D.z = ModelInfo[i]->Position2D.z;
+				TargetModel->Type = ModelInfo[i]->Type;
+				TargetModel->BaseIndex = ModelInfo[i]->BaseIndex;
+				TargetModel->MinIndex = ModelInfo[i]->MinIndex;
+				TargetModel->NumVertices = ModelInfo[i]->NumVertices;
+				TargetModel->StartIndex = ModelInfo[i]->StartIndex;
+				TargetModel->PrimitiveCount = ModelInfo[i]->PrimitiveCount;
 				minCrosshairDistance = GetDistance(ModelInfo[i]->Position2D.x, ModelInfo[i]->Position2D.y, ScreenCenterX, ScreenCenterY);
 				foundnum++;
 			}
@@ -302,27 +331,28 @@ HRESULT WINAPI EndScene_Detour(LPDIRECT3DDEVICE9 pDevice)
 
 	if (GetAsyncKeyState(0x4) && foundnum > 0 ) {
 
-		PrintText(g_font_default, minX, minY, D3DCOLOR_XRGB(0, 255, 0), "Target");
-
-		mouseOffset_X = (minX - ScreenCenterX) / mouseSmooth;
-		mouseOffset_Y = (minY - ScreenCenterY + 17) / mouseSmooth;
+		//PrintText(g_font_default, minX, minY, D3DCOLOR_XRGB(0, 255, 0), "Target");
+		CDraw.Circle(TargetModel->Position2D.x, TargetModel->Position2D.y, 20, 0, full, true, 3, LAWNGREEN(255));
+		
+		mouseOffset_X = (TargetModel->Position2D.x - ScreenCenterX) / mouseSmooth;
+		mouseOffset_Y = (TargetModel->Position2D.y - ScreenCenterY + 17) / mouseSmooth;
 
 		if (mouseOffset_X >= 50)
-			mouseOffset_X = (mouseOffset_X * mouseSmooth) / ((mouseSmooth * 0.5) < 1 ? 1 : (mouseSmooth * 0.5));
+			mouseOffset_X = (TargetModel->Position2D.x - ScreenCenterX) / ((mouseSmooth * 0.5f) < 1 ? 1 : (mouseSmooth * 0.5f));
 
+		if (mouseOffset_Y >= 50)
+			mouseOffset_Y = (TargetModel->Position2D.y - ScreenCenterY + 17) / ((mouseSmooth * 0.5f) < 1 ? 1 : (mouseSmooth * 0.5f));
 
-		if (mouseOffset_Y >= 50) 
-			mouseOffset_Y = (mouseOffset_Y * mouseSmooth) / ((mouseSmooth * 0.5) < 1 ? 1 : (mouseSmooth * 0.5));
+		mouseOffset_X += 3;
 
-		printf("ScreenCenterX:%d ScreenCenterY:%d\n", ScreenCenterX, ScreenCenterY);
-		printf("minX:%d minY:%d minDistance:%d\n", minX, minY, minCrosshairDistance);
-		printf("mouseOffset X:%d Y:%d (actual: X:%d Y:%d)\n", (minX - ScreenCenterX), (minY - ScreenCenterY), mouseOffset_X, mouseOffset_Y);
+		printf("ScreenCenterX:%f ScreenCenterY:%f\n", ScreenCenterX, ScreenCenterY);
+		printf("minX:%f minY:%f minDistance:%f\n", TargetModel->Position2D.x, TargetModel->Position2D.y, minCrosshairDistance);
+		printf("mouseOffset X:%f Y:%f (actual: X:%f Y:%f)\n", (TargetModel->Position2D.x - ScreenCenterX), (TargetModel->Position2D.y - ScreenCenterY), mouseOffset_X, mouseOffset_Y);
 		printf("\n");
 
-		mouse_event(MOUSEEVENTF_MOVE, mouseOffset_X + 1, mouseOffset_Y  , 0, 0);
+		mouse_event(MOUSEEVENTF_MOVE, mouseOffset_X, mouseOffset_Y , 0, 0);
 
 	}
-
 
 	return EndScene_Pointer(pDevice);
 }
@@ -334,34 +364,28 @@ HRESULT WINAPI DrawIndexedPrimitive_Detour(LPDIRECT3DDEVICE9 pDevice, D3DPRIMITI
 
 	if (pDevice->GetStreamSource(0, &Stream_Data, &Offset, &Stride) == D3D_OK) Stream_Data->Release();
 
-	if (fCall)
-	{
-		CreateMyShader(&Front, pDevice, 255, 0, 0, 255);
-		CreateMyShader(&Back, pDevice, 0, 255, 0, 255);
-		fCall = false;
-	}
+
 
 
 	if (func_wallhack && Stride == 32 && StartIndex == 0)
 	{
 		wallhack_ghostChams(pDevice, Type, BaseIndex, MinIndex, NumVertices, StartIndex, PrimitiveCount);
-		if (GetAsyncKeyState(VK_F8) || chahead) {
-			AddModel(pDevice);
+		if (characterHEAD) {
+			AddModel(pDevice, Type, BaseIndex, MinIndex, NumVertices, StartIndex, PrimitiveCount);
 		}
-
 	}
 
 
 	if (GetAsyncKeyState(VK_NUMPAD2) & 1)
 	{
-		mouseSmooth += 0.1;
-		cout << "mousespeed:" << mouseSmooth << endl;
+		mouseSmooth += 0.1f;
+		cout << "mouseSmooth:" << mouseSmooth << endl;
 	}
 
 	if (GetAsyncKeyState(VK_NUMPAD8) & 1)
 	{
-		mouseSmooth -= 0.1;
-		cout << "mousespeed:" << mouseSmooth << endl;
+		mouseSmooth -= 0.1f;
+		cout << "mouseSmooth:" << mouseSmooth << endl;
 	}
 
 	return DrawIndexedPrimitive_Pointer(pDevice, Type, BaseIndex, MinIndex, NumVertices, StartIndex, PrimitiveCount);
