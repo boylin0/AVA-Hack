a91c7011d9d9a73cb360887307dcf7f5e399dd5d
Gu <as20757738@gmail.com>
2018-07-27 17:08:33 +0800

    modify QQMacro.cpp -> Adjustment aimbot

--- QQMacro.cpp
+++ QQMacro.cpp
@@ -1,21 +1,24 @@
+#include <time.h>
 #include "QQMacro.h"
-
 #include "utils.h"
 
+void PressQ() {
+	keybd_event(0x51, 0, 0, 0);
+	Sleep(rand() % 25 + 1);
+	keybd_event(0x51, 0, KEYEVENTF_KEYUP, 0);
+	Sleep(rand() % 25 + 1);
+}
 
 namespace function {
 	namespace QQMacro {
 
 		void doQQMacro() {
+			Sleep(rand() % 80 + 1);
+			if (utils::isFocusOnAVA()) PressQ();
+			Sleep(rand() % 80 + 1);
+			if (utils::isFocusOnAVA()) PressQ();
+			Sleep(500);
 			//LoadKeyboardLayout((LPCWSTR)"00000409", KLF_ACTIVATE);
-			if (utils::isFocusOnAVA()) keybd_event(0x51, 0, 0, 0);
-			Sleep(150);
-			if (utils::isFocusOnAVA()) keybd_event(0x51, 0, KEYEVENTF_KEYUP, 0);
-			Sleep(100);
-			if (utils::isFocusOnAVA()) keybd_event(0x51, 0, 0, 0);
-			Sleep(25);
-			if (utils::isFocusOnAVA()) keybd_event(0x51, 0, KEYEVENTF_KEYUP, 0);
-			Sleep(625);
 
 		}
 	}
